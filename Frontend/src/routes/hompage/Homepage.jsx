febe269b33d95a5ed04121572e887b88fbad1dcd
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-07-30 14:11:06 +0600

    React Animated Homepage Design Finishing

--- Frontend/src/routes/hompage/Homepage.jsx
+++ Frontend/src/routes/hompage/Homepage.jsx
@@ -5,7 +5,24 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
   return (
     <div className='homePage'>
-      <Link to='/dashboard'>Dashboard</Link>
+      <img src="../../../public/orbital.png" className='orbital' alt="" />
+        <div className="left">
+          <h1>LAMA AI</h1>
+          <h2>Supercharge your creativity and productivity</h2>
+          <h3>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+            dolorem doloribus, architecto dolor.
+          </h3>
+          <Link to='/dashboard'>Get Started</Link>
+        </div>
+        <div className="right">
+          <div className="imgContainer">
+            <div className="bgContainer">
+              <div className="bg"></div>
+            </div>
+            <img src="./bot.png" className='bot' alt="" />
+          </div>
+        </div>
     </div>
   )
 }
